bec12831e8f30cb074f079d0ed8aba52acfeeb3a
Vitvor <112934325+Vitvor-ua@users.noreply.github.com>
2025-01-03 20:53:58 +0200

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"lab7","l":"AddExpression"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lab7","l":"DivideExpression"},{"p":"lab7","l":"Expression"},{"p":"lab7","l":"ExpressionParser"},{"p":"lab7","l":"MultiplyExpression"},{"p":"lab7","l":"NumberExpression"},{"p":"lab7","l":"SubtractExpression"}];updateSearchResults();
